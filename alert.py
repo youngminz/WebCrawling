1768ecd71221257aae77d1592e949de11313396c
Youngmin Koo <youngminz.kr@gmail.com>
2017-05-26 20:10:48 +0900

    Update alert.py

--- alert.py
+++ alert.py
@@ -1,3 +1,7 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 def alert(driver):
     try:
         WebDriverWait(driver, 1).until(expected_conditions.alert_is_present(), "")
