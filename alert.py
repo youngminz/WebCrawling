1059173048d7cbcad41409853bde93196fd87721
Youngmin Koo <youngminz.kr@gmail.com>
2017-05-26 20:15:09 +0900

    Update alert.py

--- alert.py
+++ alert.py
@@ -2,13 +2,13 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-def alert(driver):
+def is_alert_present(driver):
     try:
         WebDriverWait(driver, 1).until(expected_conditions.alert_is_present(), "")
         a = driver.switch_to.alert
         text = a.text
         a.accept()
-        return text
+        return True, text
 
     except TimeoutException:
-        return False
+        return False, ""
