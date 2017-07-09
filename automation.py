7d3f860acbd8793033db5ae80443780b62457276
Youngmin Koo <youngminz.kr@gmail.com>
2017-07-09 22:40:29 +0900

    Update automation.py

--- automation.py
+++ automation.py
@@ -4,6 +4,7 @@ class Automation:
 
     def send_keys_xpath(self, xpath, keys):
         WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((By.XPATH, xpath)))
+        self.driver.find_element_by_xpath(xpath).click()
         self.driver.find_element_by_xpath(xpath).send_keys(keys)
 
     def click_xpath(self, xpath):
