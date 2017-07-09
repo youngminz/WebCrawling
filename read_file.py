1c441454477cdb307841eae6407ce7b84917d3e3
Youngmin Koo <youngminz.kr@gmail.com>
2017-07-09 23:09:50 +0900

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -3,3 +3,10 @@ def read_txt(filename):
         for l in f:
             l = l.strip().split(",")
             yield l
+
+            
+def read_txt(filename):
+    with open(filename, "r") as f:
+        for l in f:
+            l = l.strip()
+            yield l
