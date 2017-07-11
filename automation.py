68311153efb81fb42f149371da2b25995710c535
Youngmin Koo <youngminz.kr@gmail.com>
2017-07-11 19:52:34 +0900

    Update automation.py

--- automation.py
+++ automation.py
@@ -9,7 +9,7 @@ class Automation:
         self.driver = webdriver.Chrome()
 
     def __del__(self):
-        self.driver.close()
+        self.driver.quit()
 
     def send_keys_xpath(self, xpath, keys):
         WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((By.XPATH, xpath)))
