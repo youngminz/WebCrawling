66f655223802d934b4d2cd6c878c90bcff7eafed
Youngmin Koo <youngminz.kr@gmail.com>
2017-07-14 16:06:32 +0900

    Update automation.py

--- automation.py
+++ automation.py
@@ -6,6 +6,7 @@ import zipfile
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
